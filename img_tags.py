a7f33fb239dfc42ee3207c13173bf50eb608aeef
Maxwell <msl@lclark.edu>
2019-11-17 14:03:25 -0800

    Sort images by title

--- img_tags.py
+++ img_tags.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     post_date_title = sys.argv[1]
     folder_path = "images/blog/" + post_date_title
     srcs = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
-    tags = [f"<img src='{'/' + folder_path + src}'>" for src in srcs]
+    tags = sorted([f"<img src='{'/' + folder_path + src}'>" for src in srcs])
     for tag in tags:
         print(tag)
     # print(srcs)
