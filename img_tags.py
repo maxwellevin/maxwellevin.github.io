70a10367a7b711eb65ad8bfd191d904b32894138
Maxwell <msl@lclark.edu>
2019-11-17 11:22:49 -0800

    Simplified command-line args required

--- img_tags.py
+++ img_tags.py
@@ -5,7 +5,8 @@ from os.path import isfile, join
 
 
 if __name__ == "__main__":
-    folder_path = sys.argv[1]
+    post_date_title = sys.argv[1]
+    folder_path = "images/blog/" + post_date_title
     srcs = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
     tags = [f"<img src='{'/' + folder_path + src}'>" for src in srcs]
     for tag in tags:
